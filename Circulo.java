58940897545ea81a73986bdc7a8c9dfae5f9f873
linsignares <142268649+linsignares@users.noreply.github.com>
2023-09-01 22:45:58 -0500

    Create Circulo.java

---
+++ Circulo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebafiguras;
+
+/**
+ *
+ * @author julianainsignares
+ */
+public class Circulo {
+    int radio;
+    
+    Circulo(int radio){
+        this.radio = radio;
+    }
+    
+    double calcularArea(){
+        return Math.PI*Math.pow(radio,2);
+    }
+    
+    double calcularPerimetro(){
+        return 2*Math.PI*radio;
+    }
+    
+}
